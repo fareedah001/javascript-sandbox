34df23d2abeca444b838b5de27f1106bd9f668eb
fareedah001 <ajibolafareedah02@gmail.com>
2025-02-09 09:32:07 +0100

    Add string manipulation and number handling examples with HTML structure

--- 10-challenge/script.js
+++ 10-challenge/script.js
@@ -1,1 +1,19 @@
+// let myNewString;
+// let myString = "developer";
+// myNewString =
+//   myString.char(0).myString.toUppercase() + myString.substring(1, 6);
+// console.log(myNewString);
+//
+// Create a new string called "myNewString" that holds the value of "Developer", using the lowercase value from "myString"
+const myString = "developer";
 
+let myNewString;
+
+// Solution 1:
+myNewString = myString.charAt(0).toUpperCase() + myString.substring(1);
+// Solution 2:
+myNewString = myString[0].toUpperCase() + myString.substring(1);
+// Solution 3:
+myNewString = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+
+console.log(myNewString);
