3927fc1a20121ad875e8cdce125c63bffb8499ec
fareedah001 <ajibolafareedah02@gmail.com>
2025-02-12 11:24:32 +0100

    Add HTML and JavaScript files for function scope examples with parameters

---
+++ 03-function-scope/01-fuctions-Basic/script.js
@@ -0,0 +1,17 @@
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+
+// Output: Hello!
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+add(5, 10);
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+const result = subtract(10, 5);
+console.log(result, subtract(100, 5));
